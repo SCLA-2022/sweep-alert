100179f64bfb919ae505bd28e8853e02548cb5f8
Nftdysentryanemonemitinity <bryanjeromevaliente@gmail.com>
2022-07-09 14:54:17 -0700

    drawer appears when you press your location

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import React, { useState, useEffect } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions, Pressable } from 'react-native';
 import * as Location from 'expo-location';
-import BottomDrawer from 'react-native-bottom-drawer-view';
+import BottomSheet from 'reanimated-bottom-sheet';
+import { NativeBaseProvider, Actionsheet, Box } from 'native-base';
 
 const TAB_BAR_HEIGHT = 49;
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [isOpen, setIsOpen] = useState(true);
+  const sheetRef = React.useRef(null);
+
 
   useEffect(() => {
     (async () => {
@@ -26,6 +30,7 @@ export default function App() {
   }, []);
 
   let text = 'Waiting..';
+
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
@@ -33,55 +38,72 @@ export default function App() {
   }
 
   const openDrawer = () => {
-    console.log('open drawer')
+    setIsOpen(true);
   }
-  renderContent = () => {
+
+  const onClose = () => {
+    setIsOpen(false);
+  }
+
+  const renderContent = () => {
     return (
-        <View>
-            <Text>Get directions to your location</Text>
-        </View>
+      <View>
+        <Text>Get directions to your location</Text>
+      </View>
     )
-}
+  }
   return (
-    <View style={styles.container}>
-      {location === null ? null : <MapView
-        initialRegion={{
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        style={styles.map}
-        
-      >
-        
-        
-          <Marker 
-          onPress={openDrawer}
-          coordinate={{
+    <NativeBaseProvider>
+
+
+      <View style={styles.container}>
+        {location === null ? null : <MapView
+          initialRegion={{
             latitude: location.coords.latitude,
             longitude: location.coords.longitude,
-          }} />
-       
-      </MapView>}
-      
-      <BottomDrawer
-                containerHeight={100}
-                offset={TAB_BAR_HEIGHT}
-            >
-                {this.renderContent()}
-            </BottomDrawer>
-          
-      {/* <Text style={styles.paragraph}>{text}</Text> */}
-    </View>
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+
+        >
+
+
+          <Marker
+            onPress={openDrawer}
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }} />
+
+        </MapView>}
+
+        <Actionsheet isOpen={isOpen} onClose={onClose}>
+          <Actionsheet.Content>
+            <Box w="100%" h={60} px={4} justifyContent="center">
+              <Text fontSize="16" color="gray.500" _dark={{
+                color: "gray.300"
+              }}>
+                Albums
+              </Text>
+            </Box>
+            <Actionsheet.Item>Delete</Actionsheet.Item>
+            <Actionsheet.Item>Share</Actionsheet.Item>
+            <Actionsheet.Item>Play</Actionsheet.Item>
+            <Actionsheet.Item>Favourite</Actionsheet.Item>
+            <Actionsheet.Item>Cancel</Actionsheet.Item>
+          </Actionsheet.Content>
+        </Actionsheet>
+      </View >
+    </NativeBaseProvider >
   );
-  
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
