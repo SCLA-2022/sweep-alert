0efaae04fd54112f4a39ac74a7d43c4114fee027
Michael <michaelsouliman@gmail.com>
2022-07-09 17:01:02 -0700

    created jsx page for adding car page and added search bar element

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import { StyleSheet, Text, View, Dimensions, Pressable, Button } from 'react-nat
 import * as Location from 'expo-location';
 import BottomSheet from 'reanimated-bottom-sheet';
 import { NativeBaseProvider, Actionsheet, Box } from 'native-base';
-import { SearchBar } from 'react-native-elements';
+import AddCarPage from './components/AddCarPage'
+
 
 const TAB_BAR_HEIGHT = 49;
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [search, setsearch] = useState('');
   const [isOpen, setIsOpen] = useState(true);
   const sheetRef = React.useRef(null);
 
@@ -42,34 +42,18 @@ export default function App() {
   const openDrawer = () => {
     setIsOpen(true);
   }
-  updateSearch = (search) => {
-    this.setState({ search });
-  };
-
-  function AddaCar() {
-    const { search } = this.state;
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <SearchBar
-        placeholder="Type Here..."
-        onChangeText={this.updateSearch}
-        value={search}
-      />
-      </View>
-    );
-  }
 
   const onClose = () => {
     setIsOpen(false);
   }
 
-  const renderContent = () => {
-    return (
-      <View>
-        <Text>Get directions to your location</Text>
-      </View>
+  function addCar() {
+    return(
+      <AddCarPage/>
     )
+      
   }
+
   return (
     <NativeBaseProvider>
 
@@ -106,10 +90,10 @@ export default function App() {
               <Text fontSize="16" color="gray.500" _dark={{
                 color: "gray.300"
               }}>
-                Albums
+                Functionality Checkist
               </Text>
             </Box>
-            <Actionsheet.Item>Add Car</Actionsheet.Item>
+            <Actionsheet.Item onPress={addCar}>Add Car</Actionsheet.Item>
           </Actionsheet.Content>
         </Actionsheet>
       </View >
