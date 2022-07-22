70fbac95cc29e1eebaaf8ee8ba6790170aa94c75
Nftdysentryanemonemitinity <bryanjeromevaliente@gmail.com>
2022-07-22 12:19:46 -0700

    help

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ import MapPage from './components/MapPage'
 import AddCarPage from './components/AddCarPage'
 import AllCarsPage from './components/AllCarsPage'
 import LoginPage from './components/LoginPage'
-import Timer1 from './components/Timer1'
+import CurrentAlarms from './components/CurrentAlarms'
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
 import RoutesPage from './components/RoutesPage'
 import DetailedRoutes from './components/DetailedRoutes'
 import Dashboard from './components/Dashboard'
 import usePreloadedFonts from './hooks/usePreloadedFonts';
+import SavedAddresses from './components/SavedAddresses'
  const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
@@ -28,10 +29,11 @@ const MyStack = () => {
         <Stack.Screen name="AddCarPage" component={AddCarPage} />
         <Stack.Screen name="AllCarsPage" component={AllCarsPage} />
         <Stack.Screen name="LoginPage" component={LoginPage} />
-        <Stack.Screen name="Timer1" component={Timer1} />
+        <Stack.Screen name="CurrentAlarms" component={CurrentAlarms} />
         <Stack.Screen name="RoutesPage" component={RoutesPage} />
         <Stack.Screen name="DetailedRoutes" component={DetailedRoutes} />
         <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="SavedAddresses" component={SavedAddresses} />
       </Stack.Navigator>
     </NavigationContainer>
     </GestureHandlerRootView>
--- components/SavedAddresses.jsx
+++ components/SavedAddresses.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TextInput, SafeAreaView } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import AddCarPage from "./AddCarPage"
-import CurrentAlarms from "./CurrentAlarms"
-import SavedAddresses from "./SavedAddresses"
+import Timer1 from "./Timer1"
 import {
     useFonts,
     Roboto_100Thin,
@@ -44,40 +43,6 @@ const App = ({ navigation }) => {
         />
       </View>
       <Text style={styles.DashStyle}>DASHBOARD</Text>
-      <View style={styles.viewspacing}>
-        
-        <View style={styles.boxtext}>
-          <View>
-            <Text onPress={() => navigation.navigate("AddCarPage")} style={styles.boxestext}>Save An Addresses</Text>
-          </View>
-        </View>
-        <View style={styles.boxtext}>
-          <View>
-            <Text onPress={() => navigation.navigate("SavedAddresses")} style={styles.boxestext}>View Saved Addresses</Text>
-          </View>
-        </View>
-        <View style={styles.boxtext}>
-          <View>
-            <Text onPress={() => navigation.navigate("CurrentAlarms")} style={styles.boxestext}>View Current Alarms</Text>
-          </View>
-        </View>
-        
-        <View>
-          <View style={styles.boxtext}>
-            <View>
-              <Text style={styles.boxestext}>Log Out</Text>
-            </View>
-          </View>
-          <View style={styles.minilogostyle}>
-            <View>
-              <Text style={styles.formattingone}>SWEEP
-              <Text style={styles.formattingtwo}>
-              ALERT</Text>
-              </Text>
-            </View>
-          </View>
-        </View>
-      </View>
     </View>
   );
 };
