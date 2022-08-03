bf04b8c1eb706d03a7805e1cb62f1662d72a1589
Nftdysentryanemonemitinity <bryanjeromevaliente@gmail.com>
2022-08-03 12:00:42 -0700

    detailed routes page under work

--- components/DetailedRoutes.jsx
+++ components/DetailedRoutes.jsx
@@ -1,9 +1,5 @@
-import React, {useState} from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { NativeBaseProvider } from "native-base";
@@ -65,60 +61,61 @@ const mapStyle = [
 
 export default function App({ navigation, route }) {
   const [modalVisible, setModalVisible] = useState(false);
-  console.log(route.params)
+  console.log(route.params);
 
   return (
     <NativeBaseProvider>
-    <View>
-
-
-
-      <View style = {{marginTop: 80, flexDirection: 'row', display: 'flex', justifyContent: 'center'}}>
-        <Entypo
-              onPress={() => navigation.goBack()}
-              style={[styles.IconStyle]}
-              name="chevron-left"
-              size={30}
-              color="#420F0F"
-            />
-        <View style ={{marginTop: 22}}>
-          <Text style={styles.Textheading}>Route Schedules Near</Text>
-          <Text style={styles.Textbody}>
-            3655 S Grand Ave #220,{"\n"} Los Angeles, CA 90007
-          </Text>
+      <View>
+        <View
+          style={{
+            // marginTop: 80,
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "blue",
+          }}
+        >
+          <Entypo
+            onPress={() => navigation.goBack()}
+            style={[styles.IconStyle]}
+            name="chevron-left"
+            size={30}
+            color="#420F0F"
+          />
+          <View
+            style={
+              {
+                /* marginTop: 22 */
+              }
+            }
+          >
+            <Text style={styles.Textheading}>Route Schedules Near</Text>
+            <Text style={styles.Textbody}>
+              3655 S Grand Ave #220,{"\n"} Los Angeles, CA 90007
+            </Text>
+          </View>
         </View>
-      </View>
+        <View
+          style={
+            {
+              // backgroundColor: "red",
+              // width: 200,
+              // height: 100,
+              // alignSelf: "center",
+              // paddingBottom: 56,
+              // borderWidth: 100,
+              height: 567,
+              width: 354,
+            }
+          }
+        >
+          {/* <Text style = {{height: 50}}> Time tao find out </Text> */}
 
-      {/* <Text> Does thois work </Text> */}
-      {/* style={styles.OuterView} */}
-      
-      <View style = {{backgroundColor: 'red', width: 200,  alignSelf: 'center'}}>
-
-        {/* <Text style = {{height: 50}}> Time tao find out </Text> */}
-
-      <MapView
-       initialRegion={{
-        latitude: 34.017222,
-        // location.coords.latitude,
-        longitude: -118.278205,
-        // location.coords.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }} style = {{height: 100}}>
-     
-      </MapView>
-        {/* <View> */}
-        {/* style={styles.Viewstyle} */}
-         
-            {/* <Text style={styles.headerstyle}>{route.params.day}</Text>
-            <Text style={styles.headerstyle}>{route.params.time}</Text>
-            <Text style={styles.headerstyle}>
-             {route.params.frequency}
-            </Text> */}
-            {/* <MapView
-            // style = {{flex: 1}}
-            provider={PROVIDER_GOOGLE}
-            customMapStyle={mapStyle}
+          <MapView
+            style={{ flex: 1 }}
+            // provider={PROVIDER_GOOGLE}
+            // customMapStyle={mapStyle}
+            // style={styles.map}
             initialRegion={{
               latitude: 34.017222,
               // location.coords.latitude,
@@ -127,23 +124,16 @@ export default function App({ navigation, route }) {
               latitudeDelta: 0.0922,
               longitudeDelta: 0.0421,
             }}
-            style={styles.map}
-          >
-            </MapView>
-           */}
-          {/* <View style={styles.IconView}>
-            <Entypo name="chevron-right" size={30} color="black" />
-          </View> */}
-        {/* </View> */}
-      </View>
-    
-      <View style={styles.OuterView}>
-        <View style={styles.TimerViewStyle}>
-          <Text style={styles.Timer}>Set Alarm</Text>
+            style={styles.MapViewstyle}
+          ></MapView>
+        </View>
+
+        <View style={styles.OuterView}>
+          <View style={styles.TimerViewStyle}>
+            <Text style={styles.Timer}>Set Alarm</Text>
+          </View>
         </View>
       </View>
-
-    </View>
     </NativeBaseProvider>
   );
 }
@@ -155,11 +145,21 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
+  MapViewstyle: {
+    height: 567,
+    width: 354,
+    alignSelf: "center",
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 25,
+  },
   Textheading: {
     color: "black",
     textAlign: "center",
     fontSize: 25,
     // fontWeight: "500",
+    backgroundColor: "red",
   },
   Textbody: {
     color: "black",
@@ -167,6 +167,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "300",
     fontFamily: "RobotoCondensedregular",
+    backgroundColor: "green",
   },
   Viewstyle: {
     backgroundColor: "#902E2E",
@@ -178,34 +179,36 @@ const styles = StyleSheet.create({
     padding: 5,
     flexDirection: "row",
     borderColor: "#902E2E",
+    backgroundColor: "red",
   },
   TimerViewStyle: {
     backgroundColor: "#902E2E",
     width: 200,
     height: 60,
-    borderWidth: 2,
-    borderRadius: 9,
-    marginTop: 250,
-    padding: 5,
-    flexDirection: "row",
+    borderWidth: 1,
+    borderRadius: 15,
+    // marginTop: 1,
+    // padding: 5,
+    // flexDirection: "row",
     borderColor: "#902E2E",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "green",
   },
   OuterView: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: 'red',
+    // backgroundColor: "blue",
     // width: 50,
     // height: 50,
-    flex: 1
+    flex: 1,
   },
   IconView: {
     width: 25,
     height: "100%",
     justifyContent: "flex-start",
     // alignSelf: "center",
-    alignItems: "flex-end",
+    // alignItems: "flex-end",
   },
   headerstyle: {
     fontSize: 22,
@@ -224,7 +227,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   IconStyle: {
-    right: 26,
-  }
+    // right: 26,
+    // marginHorizontal: 28,
+  },
 });
 // onPress={popup}
