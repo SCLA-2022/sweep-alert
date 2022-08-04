1d2cabeb168a568c38b1b835083bf83f3505737b
Nftdysentryanemonemitinity <bryanjeromevaliente@gmail.com>
2022-08-04 12:35:06 -0700

    icon is being blocked by something in the red box on the current alarms page

--- components/CurrentAlarms.jsx
+++ components/CurrentAlarms.jsx
@@ -11,12 +11,19 @@ const App = ({ navigation }) => {
           style={styles.arrowstyle}
           name="chevron-left"
           size={30}
-          color="black"
+          color="#420F0F"
         />
       </View>
+
       <Text style={styles.DashStyle}>CURRENT ALARMS</Text>
       <View style={styles.alignment}>
-        <Text style={styles.boxtext}>you have no current alarms :(</Text>
+        <View style={styles.boxstyle}>
+          <Text style={styles.boxtext}>
+          3655 S Grand Ave #220, Los Angeles, CA 90007
+            </Text>
+            <Entypo style={{position: "absolute", marginHorizontal: 165, zIndex: 2,}} name="chevron-up" size={24} color="white" />
+        </View>
+        {/* <Text style={styles.boxtext}>you have no current alarms :(</Text> */}
       </View>
     </View>
   );
@@ -24,49 +31,77 @@ const App = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   ViewStyle: {
-    flexDirection: "row",
-    marginVertical: 55,
+    // flexDirection: "row",
+    // marginVertical: 80,
     // backgroundColor: "red",
   },
   viewspacing: {
-    marginTop: 30,
+    marginTop: 22,
   },
   flexstyle: {
     flex: 1,
     // marginVertical: -10,
+    flexDirection: "column",
+    // backgroundColor: "red",
+
   },
   alignment: {
     justifyContent: "center", //Centered horizontally
     alignItems: "center", //Centered vertically
-    flexDirection: "row",
+    // flexDirection: "row",
+    // backgroundColor: "yellow",
+  },
+  boxstyle: {
+    // position: "absolute",
+    // zIndex: 2,
+    borderRadius: 10,
+    width: 349,
+    height: 110,
+    marginTop: 110,
+    backgroundColor: "#902E2E",
+    justifyContent: "center",
+
   },
   DashStyle: {
+    // backgroundColor: "blue",
+    position: "absolute",
+    zIndex: 1,
+    width: 210,
+    height: 29,
+    marginTop: 102,
     textAlign: "center",
     alignSelf: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
     fontSize: 25,
-    marginHorizontal: 50,
-    marginTop: -96,
-    marginBottom: 25,
+    // marginHorizontal: 50,
+    // marginTop: -96,
+    // marginBottom: 25,
     // backgroundColor: 'blue',
-    color: "#591818",
+    color: "black",
     fontWeight: "bold",
     letterSpacing: 1,
     fontFamily: "RobotoCondensedbold", //"RobotoCondensed_700Bold",
   },
   arrowstyle: {
-    marginVertical: 10,
-    marginHorizontal: 15,
+    // marginVertical: 10,
+    marginHorizontal: 19,
+    // marginTop: 80,
     color: "#591818",
-    // backgroundColor: 'yellow',
+    height: 30,
+    width: 30,
+    // backgroundColor: 'green',
+    marginTop: 80,
   },
   boxtext: {
-    alignSelf: "center",
-    justifyContent: "center",
-    fontSize: 25,
-    marginTop: 175,
-    textAlign: "center",
-    // backgroundColor: "#902E2E",
+    // alignSelf: "center",
+    fontSize: 22,
+    marginHorizontal: 20,
+    fontWeight: "500",
+    // position: "absolute",
+    color: "white",
+    fontFamily: "RobotoCondensedregular",
+    // textAlign: "center",
+    backgroundColor: "black",
     // borderWidth: ,
     fontFamily: "RobotoCondensedlight" //RobotoCondensed_300Light",
   },
